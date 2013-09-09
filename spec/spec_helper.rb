07d2f5d14fca8599f72e430462a9fc63bca271af
Stefan Wrobel <swrobel@server.fake>
2013-09-09 18:49:16 -0400

    Don't run SimpleCov on Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
-# Run Coverage report
-require 'simplecov'
-SimpleCov.start do
-  add_filter 'spec/dummy'
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Models', 'app/models'
-  add_group 'Views', 'app/views'
-  add_group 'Libraries', 'lib'
+unless ENV['TRAVIS']
+  # Run Coverage report
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'spec/dummy'
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Models', 'app/models'
+    add_group 'Views', 'app/views'
+    add_group 'Libraries', 'lib'
+  end
 end
 
 # Configure Rails Environment
